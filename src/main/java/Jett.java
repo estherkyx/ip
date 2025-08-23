ade2f358ff1d19060e55be57d17f538eedfd6d02
estherkyx <157147847+estherkyx@users.noreply.github.com>
2025-08-24 02:53:19 +0800

    Create Jett.java

---
+++ src/main/java/Jett.java
@@ -0,0 +1,14 @@
+public class Jett {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String message =
+            line + "\n" +
+            "Hello! I'm Jett\n" +
+            "What can I do for you?\n" +
+            line + "\n" +
+            "Bye. Hope to see you again soon!\n" +
+            line;
+
+        System.out.println(message);
+    }
+}
