0b645bac2995599801f8b3b92f00b6a6acac4338
estherkyx <157147847+estherkyx@users.noreply.github.com>
2025-08-24 03:14:04 +0800

    Modify Jett.java to echo user inputs

--- src/main/java/Jett.java
+++ src/main/java/Jett.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
+
 public class Jett {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String message =
-            line + "\n" +
-            "Hello! I'm Jett\n" +
-            "What can I do for you?\n" +
-            line + "\n" +
-            "Bye. Hope to see you again soon!\n" +
-            line;
+        String line = "____________________________________________________________\n";
 
-        System.out.println(message);
+        // Greeting
+        String greeting = line + "Hello! I'm Jett\n" + "What can I do for you?\n" + line;
+        System.out.println(greeting);
+
+        // User Input
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(line + userInput + "\n" + line);
+            userInput = scanner.nextLine();
+        }
+
+        // Exit
+        String exit = line + "Bye. Hope to see you again soon!\n" + line;
+        System.out.println(exit);
     }
 }
