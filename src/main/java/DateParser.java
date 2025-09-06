2b2333eb8ae5a54fd8566a2ca126e6dbe3817f9b
estherkyx <157147847+estherkyx@users.noreply.github.com>
2025-09-06 18:27:51 +0800

    Create DateParser.java

---
+++ src/main/java/DateParser.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateParser {
+    public static LocalDate parseDate(String s) {
+        String t = s.trim();
+        try {
+            return LocalDate.parse(t);
+        } catch (DateTimeParseException ignore) {}
+        try {
+            return LocalDate.parse(t, DateTimeFormatter.ofPattern("d/M/yyyy"));
+        } catch (DateTimeParseException ignore) {}
+        try {
+            return LocalDate.parse(t, DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException ignore) {}
+        throw new IllegalArgumentException();
+    }
+
+    public static String formatDate(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+}
